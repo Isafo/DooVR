b9f59f99d9c8e7788bfe4d04a5670808370cca9f
Isabelle <isafo268@student.liu.se>
2015-11-05 11:10:53 +0100

    fixing shader errors

--- doovr/ssaoF.glsl
+++ doovr/ssaoF.glsl
@@ -1,9 +1,7 @@
 #version 400
 layout( location = 0 ) out vec4 FragColor;
 
-in vec3 Position;
-in vec3 Normal;
-in vec4 uv;
+in vec3 viewNormal;
 in vec2 ssao_uv;
 in mat4 projMat;
 
@@ -11,12 +9,10 @@ uniform sampler2D depth_tex;
 //uniform sampler2D normal_tex;
 //uniform sampler2D noise_tex;
 
-uniform float Radius;
-
 // ssao Uniforms
 const int MAX_KERNEL_SIZE = 128;
 uniform int kernelSize;
-uniform float hemisphereRadius = 1.5;
+uniform float Radius = 1.5;
 uniform float power = 2.0;
 uniform vec2 noiseScale; // scale to fit the ssao tex coords to the noise kernel
 uniform mat4 sampleKernel[kernelSize];
@@ -24,7 +20,7 @@ uniform mat4 noiseKernel[kernelSize];
 
 layout(location=0) out vec4 result;
 
-float ssao(mat3 kernelBasis, vec3 origin, floar radius) {
+float ssao(mat3 kernelBasis, vec3 origin, floar Radius) {
 	
 	float occlusion = 0.0;
 
@@ -32,7 +28,7 @@ float ssao(mat3 kernelBasis, vec3 origin, floar radius) {
 	for(int i = 0; i < kernelSize; i++) {
 		// get sample position in view space
 		vec3 sample = kernelBasis * sampleKernel[i];
-		sample = sample * radius + origin;
+		sample = sample * Radius + origin;
 
 		// project the sample position back into screen space
 		vec4 offset = vec4(sample, 1.0);
@@ -41,10 +37,10 @@ float ssao(mat3 kernelBasis, vec3 origin, floar radius) {
 		offset.xy = offset.xy * 0.5 + 0.5;
 
 		//get sample depth
-		float sampleDepth = texture(ssao_depth, offset.xy).r;
+		float sampleDepth = texture(depth_tex, offset.xy).r;
 
 		// range check and accumulate
-		float rangeCheck = abs(origin.z - sampleDepth) < radius ? 1.0 : 0.0;
+		float rangeCheck = abs(origin.z - sampleDepth) < Radius ? 1.0 : 0.0;
 		occlusion += (sampleDepth <= sample.z ? 1.0 : 0.0) * rangeCheck;
 	}
 
@@ -58,15 +54,12 @@ void main () {
 	float x = ssao_uv.s * 2.0 - 1.0;
 	float y = ssao_uv.t * 2.0 - 1.0;
 
-	vec3 origin = texture(depth_tex, ssao_uv).r * 2.0 - 1.0;
+	float z = texture(depth_tex, ssao_uv).r * 2.0 - 1.0;
 	
 	vec4 projPos = vec4(x, y, z, 1.0);
-	vec4 viewPos = invert(projMat) * projPos;
+	vec4 viewPos = inverse(projMat) * projPos;
 
 	viewPos /= viewPos.w;
-
-	// calculate the view space normal
-	vec3 viewNormal = normalize(texture(normal_tex, ssao_uv).xyz * 2.0 - 1.0);
 
 	// calculate the change-of-bias mat
 	vec3 rotVec = vec3(noiseKernel[ssao_uv][ssao_uv] * noiseScale) * 2.0 - 1.0;
@@ -75,5 +68,5 @@ void main () {
 	vec3 bitangent = cross(viewNormal, tangent);
 	mat3 tbn = mat3(tangent, bitangent, viewNormal);
 
-	result = vec4(ssao(tbn, viewPos, ssaoRadius));
+	result = vec4(ssao(tbn, viewPos, Radius));
 }
--- doovr/ssaoV.glsl
+++ doovr/ssaoV.glsl
@@ -3,8 +3,9 @@ layout(location = 0) in vec3 VertexPosition;
 layout(location = 1) in vec3 VertexNormal;
 layout(location = 2) in vec2 vertexUV;
 
-out vec2 UV;
+out vec2 ssao_uv;
 out mat4 projMat;
+out vec3 viewNormal
 
 uniform mat4 MV;
 uniform mat4 P;
@@ -13,6 +14,7 @@ void main ()
 {	
 	UV = vertexUV;
 	projMat = P;
+	viewNormal = MV * VertexNormal;
 
 	// Convert position to clip coordinates and pass along to fragment shader
 	gl_Position =  (P * MV) * vec4(VertexPosition, 1.0);
